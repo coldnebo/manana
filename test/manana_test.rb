8bce5f6fe8d2066e0da1502ce571ce4a01bca1f6
Larry Kyrala <larry.kyrala@gmail.com>
2018-05-15 17:22:43 -0400

    added simplecov switched minitest to spec style

--- test/manana_test.rb
+++ test/manana_test.rb
@@ -1,11 +1,9 @@
 require "test_helper"
 
-class MananaTest < Minitest::Test
-  def test_that_it_has_a_version_number
+describe Manana do 
+
+  it "must have a version number" do 
     refute_nil ::Manana::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,12 @@
+unless ENV["TRAVIS"] == "1"
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter "/test/"
+  end
+end
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "manana"
 
 require "minitest/autorun"
+
